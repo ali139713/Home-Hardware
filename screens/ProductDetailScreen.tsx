663ffa43c1f6b6e2dd9733e061ce1cd546769a68
Ali Abdullah Azhar <aliabdullahazhar@gmail.com>
2023-01-26 00:21:41 +0500

    Implemented functionality to handle item's quantity change.

--- screens/ProductDetailScreen.tsx
+++ screens/ProductDetailScreen.tsx
@@ -1,7 +1,9 @@
 import {StackActions} from '@react-navigation/native';
 import {Button, Divider, HStack} from 'native-base';
-import React, { useEffect } from 'react';
+import React, {useCallback, useContext, useEffect, useState} from 'react';
 import {
+  Dimensions,
+  FlatList,
   Image,
   SafeAreaView,
   ScrollView,
@@ -13,64 +15,164 @@ import {appColor} from '../assets/colors';
 import Counter from '../components/Counter';
 import HorizontalImageList from '../components/HorizontalImageList';
 import IconComponent from '../components/IconComponent';
+import {Loader} from '../components/Loader';
 import Navbar from '../components/Navbar';
-import { fetchProductById } from '../helpers/ApiCall';
-import {width} from '../helpers/Constant';
-import {FONT, HEIGHT, WIDTH} from '../helpers/helperFunction';
+import OrderContext from '../context/OrderContext';
+import {fetchAllProducts, fetchProductById} from '../helpers/ApiCall';
+import {
+  FONT,
+  HEIGHT,
+  truncateText,
+  WIDTH,
+  wrapperForAllSettledPromises,
+} from '../helpers/helperFunction';
+import {notifyToast} from '../toast/toast';
 import ColorAvatars from './../components/ColorAvatars';
 import {Screens} from './../helpers/ScreenConstant';
 
-const ProductDetailScreen = ({navigation , route}: any) => {
+const {width} = Dimensions.get('window');
+const SPACING = 5;
+const ITEM_LENGTH = width * 0.8; // Item is a square. Therefore, its height and width are of the same length.
+const BORDER_RADIUS = 20;
 
-  const { productId } = route.params;
+const ProductDetailScreen = ({navigation, route}: any) => {
+  const {productId} = route.params;
+  const {addItemToCart} = useContext(OrderContext);
 
-  const images = [
-    require('../assets/onBoardingImage1.png'),
-    require('../assets/onBoardingImage2.png'),
-    require('../assets/onBoardingImage3.png'),
-  ];
 
-  const fetchData = async () => {
-    const responseOfProductById = await fetchProductById(productId)
-    console.log({responseOfProductById})
-  }
+  const [productData, setProductData] = useState<any>({});
+  const [bestSelling, setBestSelling] = useState<any[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [count, setCount] = useState<number>(1);
+
+  const getProductById = () => {
+    const responseOfProductById: any = fetchProductById(productId);
+    return responseOfProductById;
+  };
+
+  const getProducts = () => {
+    const responseOfProducts: any = fetchAllProducts();
+    return responseOfProducts;
+  };
+
+  const handleCounterPress = (type: string) => {
+    if (type === 'plus') {
+      setCount((prev: number) => prev + 1);
+    } 
+    if( type === 'minus' && count === 1){
+      return;
+    }
+    if(type === 'minus' && count > 1){
+      setCount((prev: number) => prev - 1);
+    }
+  };
 
   useEffect(() => {
-     fetchData()
-  },[])
+    async function fetchData() {
+      setLoading(true);
+      const [products, productById] = await wrapperForAllSettledPromises([
+        getProducts(),
+        getProductById(),
+      ]);
+      if (products.status === 'rejected') {
+        notifyToast('Failed to fetch products');
+      }
+      if (productById.status === 'rejected') {
+        notifyToast('Failed to fetch product by Id');
+      }
+
+      if (products.status === 'fulfilled') {
+        setBestSelling(products.value);
+      }
+      if (productById.status === 'fulfilled') {
+        setProductData(productById.value);
+      }
+      setLoading(false);
+    }
+    fetchData();
+  }, []);
 
   const handleAddToCart = () => {
+    addItemToCart({
+      id: productData.id,
+      name: productData.name,
+      image: productData.images[0].src,
+      price: parseInt(productData.price),
+      quantity:count,
+    });
     navigation.navigate(Screens.CartNavigation);
   };
 
+  const renderItem = useCallback(({item}: any) => {
+    return (
+      <View style={{width: ITEM_LENGTH}}>
+        <View style={styles.itemContent}>
+          <Image
+            source={
+              item !== null
+                ? {uri: item.src}
+                : require('../assets/placeholder.jpeg')
+            }
+            style={styles.itemImage}
+          />
+        </View>
+      </View>
+    );
+  }, []);
 
-
-  return (
+  return loading ? (
+    <Loader />
+  ) : (
     <SafeAreaView>
       <ScrollView>
         <View style={styles.container}>
-          <Navbar isFavouriteIcon={true} headerText="Room Sofa" handlePress={() => navigation.pop()} />
+          <Navbar
+            isFavouriteIcon={true}
+            headerText={
+              productData?.name?.split(' ')[0] +
+              ' ' +
+              productData?.name?.split(' ')[1]
+            }
+            handlePress={() => navigation.pop()}
+          />
           <Image
             style={styles.imageStyles}
-            source={require('../assets/onBoardingImage3.png')}
+            source={
+              productData?.images
+                ? {uri: productData.images[0].src}
+                : require('../assets/placeholder.jpeg')
+            }
           />
           <View style={styles.listContainer}>
-            {/* <HorizontalImageList data={images} /> */}
+            <View style={styles.container}>
+              <FlatList
+                data={productData.images}
+                renderItem={renderItem}
+                removeClippedSubviews
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                keyExtractor={item => item.id}
+              />
+            </View>
           </View>
           <View style={styles.categoryNameAndColorContainer}>
             <View>
-              <Text style={styles.subHeading}>Furniture</Text>
+              <Text style={styles.subHeading}>
+                {productData?.categories
+                  ? productData?.categories[0]?.name
+                  : 'Category name'}
+              </Text>
             </View>
             <ColorAvatars />
           </View>
           <View style={styles.productNameAndPriceContainer}>
-            <Text style={styles.heading}>Drawing Room</Text>
-            <Text style={styles.heading}>Sofa Set</Text>
-            <Text style={styles.subHeading}>5000$</Text>
+            <Text style={styles.heading}>{productData.name}</Text>
+            {/* <Text style={styles.heading}>Sofa Set</Text> */}
+            <Text style={styles.subHeading}>{productData.price}$</Text>
           </View>
           <View style={styles.counterAndSocialLinksContainer}>
             <View>
-              <Counter />
+              <Counter count={count} handleCounterPress={handleCounterPress} />
             </View>
             <HStack mr={2} space={3}>
               <IconComponent name="facebook" size={25} color={appColor.black} />
@@ -84,12 +186,9 @@ const ProductDetailScreen = ({navigation , route}: any) => {
           </View>
           <View style={styles.descriptionTextContainer}>
             <Text style={styles.descriptionText}>
-              Lorem ipsum dolor sit amet, consectetur adipisc,Lorem ipsum dolor
-              sit amet, consectetur adipisc,Lorem ipsum dolor sit amet,
-              consectetur adipisc,Lorem ipsum dolor sit amet, consectetur
-              adipisc,Lorem ipsum dolor sit amet, consectetur adipisc,Lorem
-              ipsum dolor sit amet, consectetur adipisc,Lorem ipsum dolor sit
-              amet, consectetur adipisc,
+              {productData.description !== ''
+                ? productData.description?.slice(3,productData.description.length -5)
+                : productData.short_description?.slice(3,productData.short_description.length -5)}
             </Text>
           </View>
           <HStack justifyContent="center">
@@ -98,14 +197,14 @@ const ProductDetailScreen = ({navigation , route}: any) => {
               bg={appColor.black}
               backgroundColor={appColor.black}
               w="90%"
-              onPress={handleAddToCart}>
+              onPress={() => handleAddToCart()}>
               Add To Cart
             </Button>
           </HStack>
           <Divider my="10" w="90%" ml={5} color={appColor.lightGray} />
           <Text style={styles.heading}>You Might Also Like</Text>
           <View style={styles.relevantProductsListContainer}>
-            {/* <HorizontalImageList data={images} isProductDetails /> */}
+            <HorizontalImageList data={bestSelling} isProductDetails />
           </View>
         </View>
       </ScrollView>
@@ -148,7 +247,7 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-end',
     alignItems: 'flex-start',
     width: '95%',
-    marginTop: HEIGHT(20),
+    marginTop: HEIGHT(0),
     marginLeft: WIDTH(10),
   },
   counterAndSocialLinksContainer: {
@@ -190,5 +289,36 @@ const styles = StyleSheet.create({
     fontWeight: '500',
     textAlign: 'justify',
     color: appColor.black,
+  },
+  itemContent: {
+    marginHorizontal: SPACING * 3,
+    alignItems: 'center',
+    backgroundColor: 'white',
+    paddingTop: 5,
+    // borderRadius: BORDER_RADIUS + SPACING * 2,
+  },
+  itemImage: {
+    width: '50%',
+    height: '80%',
+    borderRadius: BORDER_RADIUS,
+    resizeMode: 'cover',
+  },
+  productDetailContainer: {
+    alignItems: 'flex-start',
+  },
+  nameText: {
+    fontSize: FONT(14),
+    fontWeight: '600',
+    color: appColor.black,
+  },
+  priceText: {
+    fontSize: FONT(13),
+    fontWeight: '500',
+    color: appColor.gray,
+  },
+  favouriteIcon: {
+    position: 'absolute',
+    right: 50,
+    zIndex: 999,
   },
 });
