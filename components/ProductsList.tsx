b334ac999c809808f6240f33548dcaad458cb86b
Ali Abdullah Azhar <aliabdullahazhar@gmail.com>
2023-01-26 00:19:19 +0500

    implemented UI bindings.

--- components/ProductsList.tsx
+++ components/ProductsList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {FlatList, Image, StyleSheet, Text, View} from 'react-native';
 import {appColor} from '../assets/colors';
 import {FONT, HEIGHT, WIDTH} from '../helpers/helperFunction';
@@ -7,32 +7,43 @@ import IconComponent from './IconComponent';
 
 type ProductListProps = {
   data: any;
+  handleCounterPress:(type:string, item:any) => any;
 };
 
-const images = [
-  require('../assets/onBoardingImage1.png'),
-  require('../assets/onBoardingImage2.png'),
-  require('../assets/onBoardingImage3.png'),
-];
+const ProductList: React.FC<ProductListProps> = ({data,handleCounterPress}) => {
+ 
 
-const ProductList: React.FC<ProductListProps> = ({data}) => {
   return (
     <View>
       <FlatList
         data={data}
         nestedScrollEnabled
-        renderItem={({item, index}) => {
+        renderItem={({item}) => {
           return (
             <View style={styles.container}>
               <View style={styles.imageContainer}>
-                <Image style={styles.imageStyle} source={images[index]} />
+                <Image
+                  style={styles.imageStyle}
+                  source={
+                    item.image
+                      ? {uri: item.image}
+                      : require('../assets/placeholder.jpeg')
+                  }
+                />
               </View>
               <View style={styles.nameAndPriceContainer}>
-                <Text style={styles.nameText}>{item.name}</Text>
+                <Text style={styles.nameText}>
+                  {item?.name?.split(' ')[0] + ' ' + item?.name?.split(' ')[1]}
+                </Text>
                 <Text style={styles.priceText}>{item.price}</Text>
               </View>
               <View style={styles.counterContainer}>
-                <Counter flexDirection="column" iconSize={20} />
+                <Counter
+                  flexDirection="column"
+                  iconSize={20}
+                  count={item.quantity}
+                  handleCounterPress={(type:string) => handleCounterPress(type,item)}
+                />
               </View>
             </View>
           );
